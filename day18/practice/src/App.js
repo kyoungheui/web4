786ca47d4bd0950f47d878698261f1f6d9f0f5c3
kyoungheui <103475194+kyoungheui@users.noreply.github.com>
2022-08-09 07:39:30 +0900

    ..

---
+++ day18/practice/src/App.js
@@ -0,0 +1,25 @@
+import './reset.css';
+import './App.css';
+import { useState } from 'react';
+
+const text = <span>안녕하세요</span>
+const 내용 = `내용입니다 내용!`
+
+function App() {
+  const [count,setCount] = useState(0);
+
+  return (
+    <div className="App">
+      <h1>hi 메인타이틀입니다</h1>
+      <p className="hi">{count}</p>
+      <button onClick={()=>{
+        setCount(count + 1);
+      }}>증가</button>
+
+    </div>
+  );
+}
+
+export default App;
+
+// http://localhost:3000/
